a169c8982a19cf234d24472cb1eba4b47d5a1a86
Misato Uebo <misato.uebo@tongullman.co.jp>
2023-02-15 19:28:35 +0900

    CSV読み込むための実装追加

---
+++ Assets/Scripts/ReadCSV/QuestionsInfomation.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class QuestionsInfomation
+{
+    static TextAsset csvFile; // CSVファイル
+    static List<string[]> csvDatas = new List<string[]>(); // CSVの中身を入れるリスト;
+    public List<string[]> QuizList
+    {
+        get { return csvDatas; }
+    } 
+
+    public string[] Question = new string[100];
+    public string[] options1 = new string[100];
+    public string[] options2 = new string[100];
+    public string[] options3 = new string[100];
+    public string[] Answer = new string[100];
+
+    static void CsvReader()
+    {
+        csvFile = Resources.Load("Questions") as TextAsset; // Resouces下のCSV読み込み
+        StringReader reader = new StringReader(csvFile.text);
+
+        // , で分割しつつ一行ずつ読み込み
+        // リストに追加していく
+        while (reader.Peek() != -1) // reader.Peaekが-1になるまで
+        {
+            string line = reader.ReadLine(); // 一行ずつ読み込み
+            csvDatas.Add(line.Split(',')); // , 区切りでリストに追加
+        }
+
+        // csvDatas[行][列]を指定して値を自由に取り出せる
+        //Debug.Log(csvDatas[0][1]);
+    }
+
+    public void Init()
+    {
+        CsvReader();//enemyDataへ情報を一時格納
+        //各変数へデータを格納
+        for (int i = 1; i < csvDatas.Count; i++)//エネミーIDが記述された最後まで読み込み。一行目はタイトルなのでi=0はデータとして扱わない
+        {
+            Question[i] = csvDatas[i][0];
+            options1[i] = csvDatas[i][1];
+            options2[i] = csvDatas[i][2];
+            options3[i] = csvDatas[i][3];
+            Answer[i] = csvDatas[i][4];
+        }
+    }
+}
---
+++ Assets/Scripts/ReadCSV/SceneManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour
+{
+    private QuestionsInfomation questionsInfomation;
+
+    [SerializeField]
+    Text QuestionsText, options1, options2, options3 , Answer;
+    int before_index = 0;
+    int now_index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        questionsInfomation = new QuestionsInfomation();
+        questionsInfomation.Init();
+        GetQuestion();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetQuestion()
+    {
+        List<string[]> csvDataList = questionsInfomation.QuizList;
+        int quizListCount = csvDataList.Count;
+
+        bool loop = true;
+        while (loop)
+        {
+            now_index = Random.Range(1, quizListCount);
+            if (before_index == now_index) continue;
+            before_index = now_index;
+            loop = false;
+        }
+
+        QuestionsText.text = "問題 : " + questionsInfomation.Question[now_index];
+        options1.text = "選択肢1 : " + questionsInfomation.options1[now_index];
+        options2.text = "選択肢2 : " + questionsInfomation.options2[now_index];
+        options3.text = "選択肢3 : " + questionsInfomation.options3[now_index];
+        Answer.text = "答え : " + questionsInfomation.Answer[now_index];
+    }
+}
